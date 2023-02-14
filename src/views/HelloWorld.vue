3c7f09a5291953d4cfc1f7f84cfb88961effb7da
cslee <cslee@stu.xidian.edu.cn>
2023-02-14 23:35:27 +0800

    1.测试Hello World!接口

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import {onMounted, ref} from "vue";
+
+export default {
+  setup() {
+    const msg = ref("");
+
+    const hello = () => {
+      axios.get("/spc/test/jeecgDemo/hello").then((res) => {
+        console.log("success!!!");
+        console.log(res);
+        const data = res.data;
+        if (data.success) {
+          msg.value = data.result;
+        }
+      })
+    }
+
+    onMounted(() => {
+      hello();
+    });
+
+    return {
+      msg
+    };
+  }
+}
+
+</script>
